ebbd20b99e06c682db6011e39d3d1e123a2d3057
lian <lian@qq.com>
2022-08-24 15:53:14 +0800

    master

--- src/main/java/com/lz/git/GitTest.java
+++ src/main/java/com/lz/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("测试Git2");
         System.out.println("测试Git3");
         System.out.println("测试hot-fix");
+        System.out.println("master");
     }
 }
