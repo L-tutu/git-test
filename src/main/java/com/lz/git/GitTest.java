dcaeb9416fbbc20a05a61914993f3229802b6b20
lian <lian@qq.com>
2022-08-24 15:40:21 +0800

    第三次提交

--- src/main/java/com/lz/git/GitTest.java
+++ src/main/java/com/lz/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试Git");
         System.out.println("测试Git2");
+        System.out.println("测试Git3");
     }
 }
